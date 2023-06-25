e66a6d4652096c832b4a734e36dd551c62c24692
bhaskarspak <bhaskar@spakengineering.com>
2023-06-25 21:04:50 +0530

    completed the greeting part

--- src/app/common/welcome/welcome.component.ts
+++ src/app/common/welcome/welcome.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  template: ` <div class="welcome">Welcome back, {{ name }}</div> `,
+  styles: ['.welcome {color: #1F838E; font-weight: 600; font-size: 1.2rem;}'],
 })
 export class WelcomeComponent {
-
+  public name = 'Bhaskar';
 }

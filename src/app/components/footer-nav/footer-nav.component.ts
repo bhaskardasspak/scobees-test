b1b765895a2801ad700d32450aa7e8836d15215a
bhaskarspak <bhaskar@spakengineering.com>
2023-06-26 14:32:50 +0530

    added feed section and footer

--- src/app/components/footer-nav/footer-nav.component.ts
+++ src/app/components/footer-nav/footer-nav.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer-nav',
   templateUrl: './footer-nav.component.html',
-  styleUrls: ['./footer-nav.component.scss']
+  styleUrls: ['./footer-nav.component.scss'],
 })
 export class FooterNavComponent {
+  selectedNav: string = 'nav1';
 
+  selectNav(nav: string): void {
+    this.selectedNav = nav;
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 const MaterialComponents = [
   MatButtonModule,
   MatIconModule,
   MatToolbarModule,
   MatToolbarModule,
+  MatTabsModule,
+  MatCardModule,
+  MatProgressBarModule,
 ];
 
 @NgModule({
